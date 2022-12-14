e5835c4c066e4b60fb9d109c785a85444cb6f3bb
GlebGr <i21s596@students.it-college.ru>
2022-12-15 04:28:20 +0500

    Update App.js

--- UI/src/App.js
+++ UI/src/App.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
+import Admin from './Admin';
 
 
 
 export default function App() {
   return (
     <BrowserRouter>
-     <Header />
+    <Header />
     </BrowserRouter>
   );
 }
