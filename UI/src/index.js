287953d932424b1de115695c8c061ac464023d81
GlebGr <i21s596@students.it-college.ru>
2022-12-15 04:28:23 +0500

    Update index.js

--- UI/src/index.js
+++ UI/src/index.js
@@ -10,3 +10,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+const devMode = process.env.NODE_ENV === 'development';
+if (devMode && module && module.hot) {
+    module.hot.accept();
+}                             //webpack
