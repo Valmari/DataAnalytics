710460f9cd59d37d138bab58ec122f1b2722c837
GlebGr <i21s596@students.it-college.ru>
2022-12-22 21:57:59 +0500

    Update App.js

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,7 +1,10 @@
 import HomePage from "pages/HomePage";
 import LoginPage from "pages/LoginPage";
 import RegisterPage from "pages/RegisterPage";
+import ApplicForm from "pages/ApplicForm";
 import { Routes, Route } from "react-router-dom"
+
+
 
 
 function App() {
@@ -10,6 +13,7 @@ function App() {
       <Route exact path="/" element={<HomePage />} />
       <Route exact path="/login" element={<LoginPage />} />
       <Route exact path="/register" element={<RegisterPage />} />
+      <Route path="/application" element={<ApplicForm />} />
     </Routes>
   );
 }
